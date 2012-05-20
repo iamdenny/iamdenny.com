274ab390d3f87ef21f456023cc8cf958ad2cb7af
ide user ide_iamdenny <hi.iamdenny@gmail.com>
2012-05-20 14:03:26 +0000

    made some markup for facebook link

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'I am Denny. Nice to meet you.' })
+    res.render('index', { 
+        title: 'I am Denny. Nice to meet you.',
+        description : 'Welcome to Denny\'s homepage.',
+        thumbnail : 'http://iamdenny.com/images/slamdunk/t0001/1.png'
+    });
 };
